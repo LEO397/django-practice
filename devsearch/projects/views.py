adeb6afd7167f1e54d028ca9d0b2607af2e39837
Vaibhav Rathod <vaibhavdrathod@gmail.com>
2022-05-31 15:55:18 +0530

    added app and templates along with template inheritance

--- devsearch/projects/views.py
+++ devsearch/projects/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+
+def projects(request):
+    return render(request, 'projects/projects.html')
+
+
+def project(request, primaryKey):
+    return render(request, 'projects/single-project.html')
 # Create your views here.
