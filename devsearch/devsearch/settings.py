c38e9492ed60c300bd3aa40d56280afebc9bb1fc
Vaibhav Rathod <vaibhavdrathod@gmail.com>
2022-07-02 15:21:03 +0530

    static files

--- devsearch/devsearch/settings.py
+++ devsearch/devsearch/settings.py
@@ -121,6 +121,16 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+    # or it can be done as BASE_DIR / 'static' without the os.path
+]
+
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
